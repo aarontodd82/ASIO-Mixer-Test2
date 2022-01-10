bcb443d6e99f6d857994a4273566a382caf323ff
aarontodd82 <aarontodd82@gmail.com>
2022-01-09 22:05:49 -0500

    Multithreading

--- ASIO Mixer Test2/MainWindow.xaml.cs
+++ ASIO Mixer Test2/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
-﻿using NAudio.Wave;
+﻿using NAudio.Dsp;
+using NAudio.Wave;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,13 +26,31 @@ namespace ASIO_Mixer_Test2
     {
 
         #region Variables
+        protected int SampleRate = 48000;
         protected ASIO ASIO;
         protected float[] InputBuffer;
         protected float[] OutputBuffer;
-        private bool running;
+        protected BiQuadFilter[][] Filters;
 
+        protected int NumberOf_IO_Channels = 8; //In and Out must be the same (for now)
+        protected int InputChannelOffset = 0;
+        protected int OutputChannelOffset = 0;
+        protected int SamplesPerChannel = 256;
+        protected int BufferSize = 256;
+        protected float MasterVolume = 1.0f;
+
+        protected volatile bool IsMultiThreadingEnabled = false;
+        protected readonly Thread DSP_Thread;
+        protected readonly AutoResetEvent DSP_RunOnce_ARE = new AutoResetEvent(false);
+        protected readonly AutoResetEvent DSP_PassCompleted_ARE = new AutoResetEvent(false);
+        protected volatile bool DSP_AllowedToRun = true;
+
+        protected DateTime DSP_StartTime;
+        protected TimeSpan DSP_ProcessingTime;
 
         #endregion
+
+
 
         public MainWindow()
         {
@@ -42,23 +63,63 @@ namespace ASIO_Mixer_Test2
 
 
 
-           // this.CleanUp_ASIO();
+            this.Filters = new BiQuadFilter[this.NumberOf_IO_Channels][];
+            for (int i = 0; i < this.NumberOf_IO_Channels; i++)
+            {
+                this.Filters[i] = new BiQuadFilter[10];
+                this.Filters[i][0] = BiQuadFilter.HighPassFilter(this.SampleRate, 10, 3.0f);
+                this.Filters[i][1] = BiQuadFilter.PeakingEQ(this.SampleRate, 20, 1.0f, 0.3f);
+                this.Filters[i][2] = BiQuadFilter.PeakingEQ(this.SampleRate, 30, 1.0f, 0.3f);
+                this.Filters[i][3] = BiQuadFilter.PeakingEQ(this.SampleRate, 40, 1.0f, 0.3f);
+                this.Filters[i][4] = BiQuadFilter.PeakingEQ(this.SampleRate, 50, 1.0f, 0.3f);
+                this.Filters[i][5] = BiQuadFilter.PeakingEQ(this.SampleRate, 60, 1.0f, 0.3f);
+                this.Filters[i][6] = BiQuadFilter.PeakingEQ(this.SampleRate, 70, 1.0f, 0.3f);
+                this.Filters[i][7] = BiQuadFilter.PeakingEQ(this.SampleRate, 80, 1.0f, 0.3f);
+                this.Filters[i][8] = BiQuadFilter.PeakingEQ(this.SampleRate, 90, 1.0f, 0.3f);
+                this.Filters[i][9] = BiQuadFilter.PeakingEQ(this.SampleRate, 100, 1.0f, 0.3f);
+            }
 
-       
-            
 
-            this.ASIO?.Start();
+
+            this.DSP_Thread = new Thread(new ThreadStart(this.Threaded_DSP));
+            this.DSP_Thread.Start();
+
+
+
 
         }
 
-        private void OnButtonBeginClick(object sender, RoutedEventArgs e)
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            //ASIO uses unmanaged Windows OLE com sub-system, we have to dispose it
+            this.ASIO?.Dispose();
+            this.ASIO = null;
+
+            //Gracefully ask the DSP Thread to exit
+            this.DSP_AllowedToRun = false;
+            this.DSP_RunOnce_ARE.Set();
+
+            Thread.Sleep(50); //Give the DSP Thread time to exit gracefully
+            if (this.DSP_Thread.IsAlive) //If it's still running at this point, we hard abort it
+            {
+                //we don't care about Thread errors, we are closing down
+                try
+                {
+                    this.DSP_Thread.Abort();
+                }
+                catch (Exception ex)
+                {
+                    _ = ex;
+                }
+            }
+        }
+
+            private void OnButtonBeginClick(object sender, RoutedEventArgs e)
         {
             System.Diagnostics.Debug.WriteLine("Test");
 
-            int SampleRate = 96000;
-            int NumberOf_IO_Channels = 4; //In and Out must be the same (for now)
-            var InputChannelOffset = 0;
-            var OutputChannelOffset = 2; //Push channel In 0,1 to Out 2,3
+
+           
 
             //Create ASIO Device
             if (this.ASIO == null)
@@ -74,43 +135,154 @@ namespace ASIO_Mixer_Test2
                 this.InputBuffer = new float[BufferSize];
                 this.OutputBuffer = new float[BufferSize];
             }
+
+            this.ASIO?.Start();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected virtual void On_ASIO_AudioAvailable(object sender, AsioAudioAvailableEventArgs e)
         {
             //Assumes InputBuffer and OutputBuffer are pre-initialized for performance reasons
 
-            //Get the Input stream
+            this.DSP_StartTime = DateTime.Now;
+
+            //Get the ASIO input stream
             e.GetAsInterleavedSamples(InputBuffer);
 
-            #region Process the signal
-            OutputBuffer = InputBuffer; //Map the Input to the Output
 
-            //for (int i = 0; i < InputBuffer.Count(); i++)
-            //{
-            //    if (i + 2 < OutputBuffer.Length)
-            //    {
-            //        OutputBuffer[i + 2] = InputBuffer[i];
-            //    }
-            //}
+                //IDK?
+                this.Dispatcher.Invoke(() => {
+                    if (chk_Threading.IsChecked ?? true)
+                    {
+                        this.IsMultiThreadingEnabled = true;
 
-            #endregion
+                    }
+                    else
+                    {
+                        this.IsMultiThreadingEnabled = false;
 
-            //set the Output stream
+                    }
+                });
+                //IDK?
+
+
+
+            if (this.IsMultiThreadingEnabled) //this.IsMultiThreadingEnabled
+            {
+                this.DSP_RunOnce_ARE.Set(); //Run one pass of the DSP
+                this.DSP_PassCompleted_ARE.WaitOne(); //Wait until the DSP is done
+            }
+            else
+            {
+                this.DSP();
+            }
+
+            //Send OutputBuffer to ASIO Output stream
             e.SetAsInterleavedSamples(OutputBuffer);
 
-            //Legacy Support, Not used
-            e.WrittenToOutputBuffers = true;
+            this.DSP_ProcessingTime = DateTime.Now - this.DSP_StartTime;
         }
 
-        private void CleanUp_ASIO()
+        protected virtual void Threaded_DSP()
         {
-            ASIO.Stop();
-            ASIO.Dispose();
-            ASIO = null;
-            running = false;
-            buttonBegin.Content = "Begin";
+            try
+            {
+                while (true) //Keep-alive
+                {
+                    this.DSP_RunOnce_ARE.WaitOne(); //Pause the thread until signaled
+                    if (!this.DSP_AllowedToRun) //Check if we should run
+                        break;
+
+                    this.DSP_MutliThreaded();
+
+                    this.DSP_PassCompleted_ARE.Set(); //Signal that we are done
+                    this.DSP_RunOnce_ARE.Reset(); //Tell the thread it is ready to pause
+                }
+            }
+            catch (Exception ex)
+            {
+                _ = ex;
+            }
         }
+
+        protected virtual void DSP_MutliThreaded()
+        {
+            try
+            {
+                var TaskList = new List<Task>();
+                for (int ChannelIndex = 0; ChannelIndex < NumberOf_IO_Channels; ChannelIndex++)
+                {
+                    var tempChannelIndex = (int)ChannelIndex;
+                    TaskList.Add(Task.Run(() =>
+                    {
+                        this.DSP_Process_Channel(tempChannelIndex);
+                    }));
+                }
+
+                Task.WaitAll(TaskList.ToArray());
+                TaskList.Clear();
+            }
+            catch (Exception ex)
+            {
+                _ = ex;
+            }
+        }
+
+        protected virtual void DSP()
+        {
+            try
+            {
+                for (int ChannelIndex = 0; ChannelIndex < this.NumberOf_IO_Channels; ChannelIndex++)
+                {
+                    this.DSP_Process_Channel(ChannelIndex);
+                }
+            }
+            catch (Exception ex)
+            {
+                _ = ex;
+            }
+        }
+
+        protected virtual void DSP_Process_Channel(int ChannelIndex)
+        {
+            int InterleavedSampleIndex = 0;
+            if (ChannelIndex == 0 || ChannelIndex == 1)
+            {
+                for (int SampleIndex = 0; SampleIndex < SamplesPerChannel; SampleIndex++)
+                {
+                    InterleavedSampleIndex = ChannelIndex + this.NumberOf_IO_Channels * SampleIndex;
+                    var TempSample = (float)InputBuffer[InterleavedSampleIndex];
+
+                    foreach (var filter in this.Filters[ChannelIndex])
+                    {
+                        TempSample = filter.Transform(TempSample);
+                    }
+
+                    OutputBuffer[InterleavedSampleIndex + 2] = TempSample * this.MasterVolume;
+                }
+            }
+            else if (ChannelIndex > 3)
+            {
+                for (int SampleIndex = 0; SampleIndex < SamplesPerChannel; SampleIndex++)
+                {
+                    InterleavedSampleIndex = ChannelIndex + this.NumberOf_IO_Channels * SampleIndex;
+                    var TempSample = (float)InputBuffer[InterleavedSampleIndex];
+
+                    foreach (var filter in this.Filters[ChannelIndex])
+                    {
+                        TempSample = filter.Transform(TempSample);
+                    }
+
+                    OutputBuffer[InterleavedSampleIndex] = TempSample * this.MasterVolume;
+                }
+            }
+        }
+
+
 
 
     }
